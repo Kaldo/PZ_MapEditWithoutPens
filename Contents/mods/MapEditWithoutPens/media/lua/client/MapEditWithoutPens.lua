eb7fb02385ed9a76c6793a1f18679a871156b099
Kaldo <kaldoreign@gmail.com>
2022-01-21 17:57:38 +0100

    fix add note popup

--- Contents/mods/MapEditWithoutPens/media/lua/client/MapEditWithoutPens.lua
+++ Contents/mods/MapEditWithoutPens/media/lua/client/MapEditWithoutPens.lua
@@ -4,27 +4,19 @@ require "ISUI/Maps/ISTextBoxMap"
 
 local KaldoMapEdit = {}
 
-KaldoMapEdit.ISMap_CanWrite = ISMap.canWrite
 function ISMap:canWrite()
-	KaldoMapEdit.ISMap_CanWrite(self)
 	return true
 end
 
-KaldoMapEdit.ISMap_CanErase = ISMap.canErase
 function ISMap:canErase()
-	KaldoMapEdit.ISMap_CanErase(self)
 	return true
 end
 
-KaldoMapEdit.ISWorldMapSymbols_CanWrite = ISWorldMapSymbols.canWrite
 function ISWorldMapSymbols:canWrite()
-	KaldoMapEdit.ISWorldMapSymbols_CanWrite(self)
 	return true
 end
 
-KaldoMapEdit.ISWorldMapSymbols_CanErase = ISWorldMapSymbols.canErase
 function ISWorldMapSymbols:canErase()
-	KaldoMapEdit.ISWorldMapSymbols_CanErase(self)
 	return true
 end
 
@@ -52,6 +44,7 @@ function ISWorldMapSymbols:checkInventory()
 			self.currentColor = self.blackColor
 		end
 	end
+	
 	self:updateSymbolColors()
 	for _,symbolButton in ipairs(self.buttonList) do
 		symbolButton.enable = true
@@ -71,27 +64,31 @@ function ISWorldMapSymbols:checkInventory()
 end
 
 
--- KaldoMapEdit.ISTextBoxMap_updateButtons = ISTextBoxMap.updateButtons
--- function ISTextBoxMap:updateButtons()
-	-- KaldoMapEdit.ISTextBoxMap_updateButtons(self)
-	
-	-- for _,colorBtn in ipairs(self.colorButtons) do
-        -- colorBtn.enable = true
-    -- end
+KaldoMapEdit.ISTextBoxMap_updateButtons = ISTextBoxMap.updateButtons
+function ISTextBoxMap:updateButtons()
+	KaldoMapEdit.ISTextBoxMap_updateButtons(self)
     
-    -- self.yes:setEnable(true);
-    -- self.yes.tooltip = nil;
-    -- local text = self.entry:getText()
-    -- if self.validateFunc and not self.validateFunc(self.validateTarget, text, self.validateArgs[1], self.validateArgs[2]) then
-        -- self.yes:setEnable(false);
-        -- self.yes.tooltip = self.validateTooltipText;
-    -- end
-    -- if self.maxChars and self.entry:getInternalText():len() > self.maxChars then
-        -- self.yes:setEnable(false);
-        -- self.yes.tooltip = getText("IGUI_TextBox_TooManyChar", self.maxChars);
-    -- end
-    -- if self.noEmpty and string.trim(self.entry:getInternalText()) == "" then
-        -- self.yes:setEnable(false);
-        -- self.yes.tooltip = getText("IGUI_TextBox_CantBeEmpty");
-    -- end
--- end
+	local currentEnabled = nil
+	local firstEnabled = nil
+	for _,colorBtn in ipairs(self.colorButtons) do
+		colorBtn.enable = true
+		colorBtn.borderColor.a = 0.4 -- not selected
+		firstEnabled = firstEnabled or colorBtn
+		if colorBtn.buttonInfo.colorInfo:equals(self.currentColor) then
+			currentEnabled = colorBtn
+			colorBtn.borderColor.a = 1.0
+		end
+		colorBtn.tooltip = nil
+	end
+	if not currentEnabled then
+		if firstEnabled ~= nil then
+			firstEnabled.borderColor.a = 1.0 -- selected
+			self.currentColor = firstEnabled.buttonInfo.colorInfo
+		else
+			self.currentColor = self.blackColor
+		end
+	end
+
+    self.yes:setEnable(true);
+    self.yes.tooltip = nil;
+end
